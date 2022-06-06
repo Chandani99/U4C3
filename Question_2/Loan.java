6bfaafc82aa572ca49d7db2a8acb9286118a579b
Chandani99 <er.chandni99@gmail.com>
2022-06-06 16:09:39 +0530

    modified

--- Question_2/Loan.java
+++ Question_2/Loan.java
@@ -8,9 +8,12 @@ public class Loan {
 	 public double calculateLoanAmount(Employee employeeObj) {
 		 if(employeeObj instanceof PermanentEmployee) {
 			 
+			 double loan=employeeObj.getSalary()*15/100;
+			 return loan;
 		 }
 		 else if(employeeObj instanceof TemporaryEmployee) {
-			 
+			 double loan=employeeObj.getSalary()*15/100;
+			 return loan;
 		 }
 		return 0;
 	    	
--- Question_2/Main.java
+++ Question_2/Main.java
@@ -8,13 +8,28 @@ public class Main {
 		Employee em1=new TemporaryEmployee(201,"xyz",500,5);
 		
 		Employee em2=null;
+		
 		Loan l=new Loan();
 		
-		l.calculateLoanAmount(em);
-		l.calculateLoanAmount(em1);
-		l.calculateLoanAmount(em2);
+		double result =l.calculateLoanAmount(em);
+		double result1=l.calculateLoanAmount(em1);
+		double result2 =l.calculateLoanAmount(em2);
 		
-
+		System.out.println("Permanent Employee Details========= " );
+		System.out.println("Id: " +em.getEmployeeId());
+		System.out.println("Name: " +em.getEmployeeName());
+		System.out.println("Salary: " +em.getSalary());
+		System.out.println("Loan of permanent Employee: "+ result );
+		
+		System.out.println("Temporary Employee Details========= " );
+		System.out.println("Id: " +em1.getEmployeeId());
+		System.out.println("Name: " +em1.getEmployeeName());
+		System.out.println("Salary: " +em1.getSalary());
+		System.out.println("Loan of permanent Employee: "+ result1 );
+		
+		
+		System.out.println("Null Employee Details========= " );
+		System.out.println("Loan of permanent Employee: "+ result2 );
 	}
 
 }
